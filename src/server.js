dc8bb8af3e71c4dfa55d7363ce2b2904210561d4
JhenifferMoraes <jheniffermoraes7@gmail.com>
2026-04-27 17:28:45 -0300

    ✨ feat: configura servidor express
    Implementa o ponto de entrada da aplicação. Configura middleware JSON, roteamento de funcionários e porta via variáveis de ambiente.

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import funcionarioRoutes from './routes/funcionarioRoutes.js';
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use('/funcionarios', funcionarioRoutes);
+
+
+// questão de segurança para não expor a porta em caso de subir os codigos para um repositório público,
+//  por exemplo, o que é uma boa prática.
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
