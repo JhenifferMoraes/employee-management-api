1d05c6270aecb1f7c22bdcb7e67a089cf8e1f4ae
JhenifferMoraes <jheniffermoraes7@gmail.com>
2026-04-27 23:19:07 -0300

    ✨ feat: integra CRUD funcionarios
    Implementa consumo da API de funcionarios via fetch.
    
    Adiciona listagem dinâmica em tabela com renderização DOM.
    
    Inclui criação e edição com POST e PUT.
    
    Implementa ações de editar e excluir na interface.

---
+++ src/public/script.js
@@ -0,0 +1,79 @@
+const API_URL = 'http://localhost:3000/funcionarios';
+
+// configuração para poder receber a resposta do servidor 
+async function carregarFuncionarios(){
+    
+    // pausa a execução da função até que a resposta do servidor seja recebida
+    const response = await fetch(API_URL);
+    // converte a resposta do servidor para um formato que possa ser manipulado em JavaScript
+    const funcionarios = await response.json();
+
+    const tabela = document.getElementById('tabela-funcionarios');
+    tabela.innerHTML = '';
+
+
+   
+    funcionarios.forEach(func =>{
+        // foi criado uma variavel para chamar cada funcionario e exibir seus dados na tabela
+        const tr = document.createElement('tr');
+
+        //um objeto (func) é criado para cada funcionário e seus dados 
+        // são inseridos na tabela usando a propriedade innerHTML
+
+        //criação dos botões de editar e excluir para cada funcionário
+
+        tr.innerHTML = `
+            <td>${func.id}</td>
+            <td>${func.nome}</td>
+            <td>${func.cargo}</td>
+            <td>${parseFloat(func.salario).toFixed(2)}</td>
+            <td>
+                <button onclick="editarFuncionario(${func.id}, 
+                ${func.nome}, ${func.cargo}, ${func.salario})">Editar</button>
+                <button onclick="excluirFuncionario(${func.id})">Excluir</button>
+            </td>
+            `;
+            tabela.appendChild(tr);
+    });
+};
+// "e" de prevent default, para evitar que a página seja recarregada ao enviar o formulário 
+document.getElementbyId('funcionarioForm').addEventListener('submit', async (e) =>{
+    e.preventDefault();
+// os valores dos campos do formulário são obtidos usando o método getElementById e 
+// armazenados em variáveis
+    const id = document.getElementById('id').value;
+    const nome = document.getElementById('nome').value;
+    const cargo = document.getElementById('cargo').value;
+    const salario = document.getElementById('salario').value;
+// um objeto é criado para armazenar os dados do funcionário, que serão enviados para o servidor
+    const funcionario = { 
+        nome,
+        cargo,
+        salario
+    };
+
+    if(id){
+
+        await fetch(`${API_URL}/${id}`, {
+
+            method: 'PUT',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(funcionario)
+            });
+        
+
+    }else{
+        await fetch(API_URL, {
+
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(funcionario)
+          
+        });
+    }
+
+    e.target.reset();
+    document.getElementById('id').value = '';
+    carregarFuncionarios();
+
+});
